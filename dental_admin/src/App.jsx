9dad9f50c0ba36b931ae4ab70d386322887c5b89
smit <smitbharshankar110@gmail.com>
2025-01-03 17:02:40 +0530

    add toastify

--- dental_admin/src/App.jsx
+++ dental_admin/src/App.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
@@ -15,9 +16,45 @@ const ProtectedRoute = ({ children }) => {
 };
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const isMobileDevice = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    setIsMobile(isMobileDevice);
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="text-3xl text-center">
+          Please switch to a desktop to view the contents
+        </h1>
+      </div>
+    );
+  }
+
   return (
     <div className="h-screen flex">
       <BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        toastStyle={{
+          maxWidth: '300px',
+          borderRadius: '4px',
+          fontSize: "0.875rem",
+          padding: "8px",
+          color: '#050505',
+          fontFamily: 'sans-serif',
+        }}
+      />
         <div className="w-full h-auto flex-1 flex flex-col">
           <Navbar />
           <div className="flex flex-row">
