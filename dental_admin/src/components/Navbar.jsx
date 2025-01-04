56d83a0654cdf6064500aa7532fcda6273ef00aa
smit <smitbharshankar110@gmail.com>
2025-01-04 12:42:54 +0530

    conflict

--- dental_admin/src/components/Navbar.jsx
+++ dental_admin/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className='z-10 w-full bg-gray-800 text-white sticky top-0'>
       <div className='w-full flex flex-row justify-between items-center px-10'>
         <img src={logo} alt="" className='h-16 w-16 rounded-xl' />
-        <button>Login</button>
+        <div>Login</div>
       </div>
     </div>
   )
